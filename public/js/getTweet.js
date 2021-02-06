30524bc6798937f2b3f92957396678f607e033c3
Mohammad Khanverdizadeh <36243199+Moe-Verdizadeh@users.noreply.github.com>
2021-02-06 18:29:19 -0500

    push

--- public/js/getTweet.js
+++ public/js/getTweet.js
@@ -13,25 +13,19 @@ function gettingTweets(){
         q: 'stocks',
         count: 10,
     }; 
+    
     T.get('search/tweets', param, gotData);
 
     function gotData(err, data, response){  
-        var tweets = data.statuses; 
-        // for(var i = 0; i < tweets.length; i++){
-        //     eachTweets = tweets[i].text;
-            // console.log('Tweets = ', eachTweets);
-            // return eachTweets;
-            // return tweets[i].text;
-            // console.log('Tweets = ', tweets[i].text); 
-        // }
+        var tweets = data.statuses;  
         var tweetList = [];
         tweets.forEach(gettingTweets); 
         function gettingTweets(eachTweets, index) {
             // console.log('tweets ' + index, eachTweets.text);
             tweetList = eachTweets.text;
             console.log( 'tweet list ' + index, tweetList);
-            document.getElementById("tweetContent").innerHTML += index + ":" + eachTweets ;
-        } 
+            document.getElementsByClassName('.tweetContent').innerHTML
+        }  
     }  
 }
 
