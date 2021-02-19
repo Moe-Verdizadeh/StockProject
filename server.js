a961156ccd46cc5eca89f99a39ac6cf0da6f8f80
Mohammad <mohammad.khanverdizadeh@gmail.com>
2021-02-19 12:37:39 -0500

    adding packages

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 const expressLayouts =  require("express-ejs-layouts");  
+
 
 const indexRouter = require('./routes/index');  
 
@@ -8,6 +13,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 
+app.use(morgan('tiny')); 
 
 app.use(expressLayouts);
 app.use(express.static('public'));
@@ -15,8 +21,19 @@ app.use(express.static('public'));
 app.use('/', indexRouter);
 app.use('/about', indexRouter);
 
-// app.get('*', (req,res) => {
-//     res.sendFile(path.join(__dirname + "/client/build/index.html"));
-// });
+app.get('/api', (req,res) => {
+    const data = {
+        userName: 'moe',
+        age: 5
+    }
+    res.json(data);
+});
+app.get('/api/name', (req,res) => {
+    const data = {
+        userName: 'mohammad',
+        age: 55
+    }
+    res.json(data);
+});
  
 app.listen(process.env.PORT || 8000);
