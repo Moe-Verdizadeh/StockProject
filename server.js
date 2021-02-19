9037055246621d6735a0fc0d9a72c581d53525b7
Mohammad <mohammad.khanverdizadeh@gmail.com>
2021-02-19 15:11:40 -0500

    dealing with mongo

--- server.js
+++ server.js
@@ -6,17 +6,33 @@ const path = require('path');
 const app = express();
 const expressLayouts =  require("express-ejs-layouts");  
 
-
 const indexRouter = require('./routes/index');  
+
+const mongoURL = 'mongodb+srv://mokode:TeddyMo3@twitterdb.bcxkk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+// mongoose.connect(mongoURL , {
+//     useNewUrlParser: true,
+//     // useUnifiedTopology: true
+// });
+mongoose.connect(mongoURL, { 
+    useNewUrlParser: true
+}).catch(err =>console.error(`ERROR: ${err}`));
+//end mangoose
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is Connected!'); 
+})
+
+app.use(morgan('tiny')); 
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 
-app.use(morgan('tiny')); 
-
 app.use(expressLayouts);
 app.use(express.static('public'));
+
  
 app.use('/', indexRouter);
 app.use('/about', indexRouter);
@@ -27,13 +43,6 @@ app.get('/api', (req,res) => {
         age: 5
     }
     res.json(data);
-});
-app.get('/api/name', (req,res) => {
-    const data = {
-        userName: 'mohammad',
-        age: 55
-    }
-    res.json(data);
-});
+}); 
  
 app.listen(process.env.PORT || 8000);
